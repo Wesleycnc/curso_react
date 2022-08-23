8dc282135ac006cfbf531b1939e471631bb17d3b
Wesleycnc <wesleycaldas728@gmail.com>
2022-08-23 12:20:16 -0300

    Initial commit

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <main>
+         <h1>Hello world React</h1>
+         <p>Mudar a cor</p>
+      </main>
+
+      
+    </div>
+  );
+}
+
+export default App;
+
+

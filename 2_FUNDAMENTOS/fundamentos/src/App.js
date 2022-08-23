f247cc9c27b7e159641c4ef1823118edaaa52a2c
Wesleycnc <wesleycaldas728@gmail.com>
2022-08-23 12:35:11 -0300

    Importação de componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// components
+import FirstComponents from './components/FirstComponents'
+
+//styles / css
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>Fundamentos React</h1>
+        <FirstComponents />
     </div>
   );
 }
